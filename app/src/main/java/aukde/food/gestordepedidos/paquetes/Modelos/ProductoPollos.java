ba8dc0278846c13ae4be5feebc6e2863b58c4abd
dannyroot <megamanxxx678@gmail.com>
2020-08-31 18:47:15 -0500

    Version 10

---
+++ app/src/main/java/aukde/food/gestordepedidos/paquetes/Modelos/ProductoPollos.java
@@ -0,0 +1,4 @@
+package aukde.food.gestordepedidos.paquetes.Modelos;
+
+public class ProductoPollos {
+}
---
+++ app/src/main/java/aukde/food/gestordepedidos/paquetes/Productos/AgregarProductoPorDefecto.java
@@ -0,0 +1,19 @@
+package aukde.food.gestordepedidos.paquetes.Productos;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import aukde.food.gestordepedidos.R;
+
+public class AgregarProductoPorDefecto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppThemeRedCake);
+        setContentView(R.layout.activity_agregar_producto_por_defecto);
+    }
+
+
+
+}
