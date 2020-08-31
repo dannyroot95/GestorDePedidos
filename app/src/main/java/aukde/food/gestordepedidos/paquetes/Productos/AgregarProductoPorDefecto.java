3c6d4a080698bfce34deb70085a83fcd0a65a218
dannyroot <megamanxxx678@gmail.com>
2020-08-31 18:58:41 -0500

    Version 12

--- app/src/main/java/aukde/food/gestordepedidos/paquetes/Productos/AgregarProductoPorDefecto.java
+++ app/src/main/java/aukde/food/gestordepedidos/paquetes/Productos/AgregarProductoPorDefecto.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import aukde.food.gestordepedidos.R;
+import aukde.food.gestordepedidos.paquetes.Inclusiones.MiToolbar;
 
 public class AgregarProductoPorDefecto extends AppCompatActivity {
 
@@ -12,6 +13,7 @@ public class AgregarProductoPorDefecto extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTheme(R.style.AppThemeRedCake);
         setContentView(R.layout.activity_agregar_producto_por_defecto);
+        MiToolbar.Mostrar(this,"Agregar producto",false);
     }
 
 
