be3c603f277f332f06f2e557e2c2d4cbec40c808
dannyroot <megamanxxx678@gmail.com>
2020-09-08 02:45:19 -0500

    Version 12

---
+++ app/src/main/java/aukde/food/gestordepedidos/paquetes/Productos/Default/DetalleProductoDefault.java
@@ -0,0 +1,19 @@
+package aukde.food.gestordepedidos.paquetes.Productos.Default;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import aukde.food.gestordepedidos.R;
+
+public class DetalleProductoDefault extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.AppThemeRedCake);
+        setContentView(R.layout.activity_detalle_producto_default);
+
+
+
+    }
+
+}
