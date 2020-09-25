de30610913247349019d29b23941cb1119bd98a4
Jose Armando ccama <ccamacruz2018@gmail.com>
2020-09-25 12:40:10 -0500

    Version 13

---
+++ app/src/main/java/aukde/food/gestordepedidos/Editar_Administrador.java
@@ -0,0 +1,14 @@
+package aukde.food.gestordepedidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Editar_Administrador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_administrador);
+    }
+}
