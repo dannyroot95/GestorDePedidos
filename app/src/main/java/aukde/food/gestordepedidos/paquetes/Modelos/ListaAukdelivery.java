4069a8116012ebcb07696d4a6a5d6678ce57cfc7
Jose Armando ccama <ccamacruz2018@gmail.com>
2020-09-25 12:21:28 -0500

    Version 13

---
+++ app/src/main/java/aukde/food/gestordepedidos/paquetes/Modelos/ListaAukdelivery.java
@@ -0,0 +1,143 @@
+package aukde.food.gestordepedidos.paquetes.Modelos;
+
+import java.io.Serializable;
+
+public class ListaAukdelivery implements Serializable {
+
+    String id;
+    String nombres;
+    String apellidos;
+    String username;
+    String dni;
+    String telefono;
+    String marcademoto;
+    String placa;
+    String categoria_licencia;
+    String numero_Licencia;
+    String email;
+    String soat;
+    String foto;
+
+    public ListaAukdelivery() {
+
+    }
+    public ListaAukdelivery(String id, String nombres, String apellidos, String username, String dni, String telefono, String marcademoto, String placa, String categoria_licencia, String numero_Licencia, String email, String soat, String foto) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.username = username;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.marcademoto = marcademoto;
+        this.placa = placa;
+        this.categoria_licencia = categoria_licencia;
+        this.numero_Licencia = numero_Licencia;
+        this.email = email;
+        this.soat = soat;
+        this.foto = foto;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getMarcademoto() {
+        return marcademoto;
+    }
+
+    public void setMarcademoto(String marcademoto) {
+        this.marcademoto = marcademoto;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCategoria_licencia() {
+        return categoria_licencia;
+    }
+
+    public void setCategoria_licencia(String categoria_licencia) {
+        this.categoria_licencia = categoria_licencia;
+    }
+
+    public String getNumero_Licencia() {
+        return numero_Licencia;
+    }
+
+    public void setNumero_Licencia(String numero_Licencia) {
+        this.numero_Licencia = numero_Licencia;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSoat() {
+        return soat;
+    }
+
+    public void setSoat(String soat) {
+        this.soat = soat;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
---
+++ app/src/main/java/aukde/food/gestordepedidos/paquetes/Modelos/ListaProveedor.java
@@ -0,0 +1,157 @@
+package aukde.food.gestordepedidos.paquetes.Modelos;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ListaProveedor implements Serializable {
+
+    String id;
+    String nombres;
+    String apellidos;
+    String username;
+    String dni;
+    String telefono;
+    String direccion;
+    String categoria;
+    String nombreempresa;
+    String ruc;
+    String email;
+    String latitud;
+    String longitud;
+    String foto;
+
+    public ListaProveedor(){
+
+    }
+    public ListaProveedor(String id, String nombres, String apellidos, String username, String dni, String telefono, String direccion, String categoria, String nombreempresa, String ruc, String email, String latitud, String longitud, String foto) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.username = username;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.categoria = categoria;
+        this.nombreempresa = nombreempresa;
+        this.ruc = ruc;
+        this.email = email;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.foto=foto;
+
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreempresa;
+    }
+
+    public void setNombreEmpresa(String nombreempresa) {
+        this.nombreempresa = nombreempresa;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+
+    public String getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        this.longitud = longitud;
+    }
+
+
+}
